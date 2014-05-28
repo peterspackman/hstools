6b6e5c9081b0d0b0d6ec66228dc1fa5895edc649
Peter Spackman <20265845@student.uwa.edu.au>
2014-05-28 13:40:56 +0800

    restructured the way get_correl_mat calculates

--- calc.py
+++ calc.py
@@ -4,14 +4,14 @@ import sys
 import numpy as np
 import cv2
 import time
+
 import matplotlib as mpl
 from matplotlib import pyplot as plt
-import scipy.stats as stats
-import hist  as h
 import scipy.spatial.distance
 import fastcluster as fc
 from scipy.cluster.hierarchy import dendrogram
-
+import scipy.stats as stats
+import hist  as h
 
 
 def spearman_roc(hist1, hist2):
@@ -19,18 +19,19 @@ def spearman_roc(hist1, hist2):
   This may need to be modified, I'm uncertain whether or not 2 zeroes are ignored
   READ likely that they aren't, as such artificially high correlations are probable
   """ 
+
   x = hist1.flatten()
   y = hist2.flatten()
-  return stats.spearmanr(x,y)
+ 
+  r,p = stats.spearmanr(x,y)
+  return r
 
 def kendall_tau(hist1, hist2):
   """ Calculate Kendall's Tau from the given histograms"""
   x = hist1.flatten()
   y = hist2.flatten()
-
-  return stats.kendalltau(x,y)
-
-
+  r,p = stats.kendalltau(x,y)
+  return r 
 
 def get_correl_mat(histograms,test=spearman_roc):
   """ SHOULD BE RENAMED TO get_dist_mat
@@ -46,12 +47,49 @@ def get_correl_mat(histograms,test=spearman_roc):
   print "Creating a {0}x{0} matrix using coefficients from {1}".format(n,test.__name__)
   start_time = time.time()
 
+  # this will be O(n^2) but I don't see a way around that
+  mat = [[test(np.array(H1),np.array(H2)) for H1,_,_ in histograms]  \
+          for i,(H2,_,_) in enumerate(histograms)]
+  
+  mat = np.array(mat)
+
+  # Because these tests give correlations not distances i.e. higher r => closer,
+  # we must modify the values to give a distance equivalent
+  
+  if test is spearman_roc or test is kendall_tau:
+    # np.round() is used here because of floating point rounding (getting 1.0 - 1.0 != 0.0)
+    mat = 1.0 - np.round(mat,decimals=5)
+  print 'matrix took {0} seconds to create'.format(time.time() - start_time)
+  return mat
+
+
+
+def get_correl_mat_slow(histograms,test=spearman_roc):
+  """ SHOULD BE RENAMED TO get_dist_mat
+      Given a list of histograms, calculate the distances between them
+      and return a NxN redundant array of these distances
+
+      It should be noted that there is a potential inefficiency 
+      here as x-> should be the same as y->x, so we could cut cpu time in
+      half roughly but cutting out the inefficiency.
+  """
+  n = len(histograms)
+
+  print "Creating a {0}x{0} matrix using coefficients from {1}".format(n,test.__name__)
+  start_time = time.time()
+
   mat = np.zeros( (n,n) )
-    
+ 
+
+  # this will be O(n^2) but I don't see a way around that
+
   for i, (H1,x1,y1) in enumerate(histograms):
     for j, (H2,x2,y2) in enumerate(histograms):
-      r, p = test(np.array(H1),np.array(H2))
-      mat[i][j] = r
+      if(mat[j][i]):
+        mat[i][j] = mat[j][i]
+      else:
+        r, p = test(np.array(H1),np.array(H2))
+        mat[i][j] = r
   # Becuase these tests give correlations not distances i.e. higher r => closer,
   # we must modify the values to give a distance equivalent
   
