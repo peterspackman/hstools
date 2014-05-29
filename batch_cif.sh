6a1694de7e1b0c9e3c108030bc982b0166fb2fcb
Peter Spackman <20265845@student.uwa.edu.au>
2014-05-29 13:05:36 +0800

    restructured batch_cif to be more readable/maintainable, added TODO

--- batch_cif.sh
+++ batch_cif.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 # This script is designed to run multiple instances of tonto
 # in order to calculate .cxs files from .cif inputs
-
+EXE=/home/prs/phd/run_cif_to_surface.exe
 LOG=batch_cif.log
+RES=0.25
 DIR=$1
-parallel  --joblog $LOG --progress '~/phd/run_cif_to_surface.exe -res 0.5 -cif {} -cxs {.}.cxs > {.}.out' ::: $DIR/*.cif
+JOB="$EXE -res $RES -cif {} -cxs {.}.cxs > {.}.out"
+
+parallel  --joblog $LOG --progress $JOB ::: $DIR/*.cif
