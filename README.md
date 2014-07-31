35ad5d27c1ab036b4b54f5fc64c6bb70ab4ac70a
Peter Spackman <20265845@student.uwa.edu.au>
2014-07-31 12:50:12 +0800

    updated readme to md, added grabpoints.py

---
+++ README.md
@@ -0,0 +1,17 @@
+## Simple command line computational chemistry (SCLCC)
+The aim of this project is to provide a simple interface
+for a variety of computational techniques associated
+with hirshfeld surface calculations in tonto.
+
+This software has only been tested using python 2.7.
+
+A list of contributors is available in contributors.txt
+Dependencies:
+* python-progressbar2
+* fastcluster
+* numpy
+* scipy
+* matplotlib
+* docopt
+
+Installing these dependencies is straightforward using pip or easy_install
---
+++ grabpoints.py
@@ -0,0 +1,21 @@
+import sys
+
+vertices = 0
+linesout = []
+
+with open(sys.argv[1]) as f:
+    lines = f.readlines()
+    for i, line in enumerate(lines):
+        if vertices > 0:
+            linesout.append(line)
+            vertices = vertices - 1
+        if(line.strip().startswith('begin vertices ')):
+            vertices = int(line.split()[2])
+            print 'Found vertices: {0}'.format(vertices)
+        if vertices < 0:
+            break
+
+
+with open(sys.argv[2], 'w') as f:
+    for line in linesout:
+        f.write(line)
