96d3d33320341ccbdd8e8fea89bdd7bceb4baeb7
Peter Spackman <20265845@student.uwa.edu.au>
2014-09-23 15:14:03 +0800

    removed c extensions no longer in use

--- pack/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension
-import numpy.distutils.misc_util
-
-setup(
-    ext_modules=[Extension("cio", ["cio.c", "readcxsfile.c", "cross.c"])],
-    include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs())
-
