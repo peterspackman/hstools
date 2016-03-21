8f3660282417cd8708fc02d871a888f395e9b787
Peter Spackman <peterspackman@fastmail.com>
2016-03-21 15:49:30 +0800

    Updated for first (barely) working Z > 1 crystals

---
+++ src/main.f90
@@ -0,0 +1,114 @@
+subroutine process_command_line(command_line, cif, hdf, res, l_max, basis)
+    use TYPES_MODULE, only: COMMAND_LINE_TYPE
+    use COMMAND_LINE_MODULE, only: process_options, has_arguments, destroy_ptr_part_
+    use SYSTEM_MODULE, only: die_if, tonto
+    use STR_MODULE, only: to_real, to_int
+
+    implicit none
+
+    type(COMMAND_LINE_TYPE), intent(in) :: command_line
+    character(len=512), intent(out) :: cif, hdf, basis
+    real(8), intent(out) :: res
+    integer(4), intent(out) :: l_max
+
+    integer(4) :: a
+    character(len=512) :: val, option
+
+    ! Get the command line
+    call process_options(command_line)
+    call die_if(tonto, has_arguments(command_line), &
+        "error: illegal arguments; use options only")
+
+    ! Default options
+    cif = "input.cif"
+    hdf = "output.h5"
+    basis = "./basis_sets"
+    res = 0.5
+    l_max = 10
+    ! Analyze command line options
+
+    do a = 1,command_line%n_options
+
+        ! Get options
+        option = command_line%option(a)
+        val = command_line%option_value(a)
+
+        ! Analyze options
+        select case (option)
+        case("i");  cif = val
+        case("o");  hdf = val
+        case("r");  res = to_real(val)
+        case("l");  l_max = to_int(val)
+        case("b"); basis = trim(val)
+        case default; print *, "unknown option: ", trim(option)
+        end select
+
+    end do
+
+    call destroy_ptr_part_(command_line)
+end subroutine
+
+program hirshfeld_surface
+    use TYPES_MODULE, only: COMMAND_LINE_TYPE, MOLECULE_TYPE, SPHERICAL_TYPE
+    use SYSTEM_MODULE, only: system_create => create_, system_destroy => destroy_, &
+        tonto
+    use CIF_MODULE, cif_create => create_, cif_destroy_ => destroy
+      use INTERPOLATOR_MODULE, only: interpolator_create => create_, &
+        interpolator_destroy => destroy_
+    use MOLECULE_BASE_MODULE, only: create_, destroy_
+    use MOLECULE_CE_MODULE
+    use MOLECULE_MAIN_MODULE, only: cleanup
+    use MOLECULE_PLOT_MODULE
+    use MOLECULE_XTAL_MODULE
+    use PLOT_GRID_MODULE, only: plot_gridcreate => create_
+    use REAL_MODULE
+    use STR_MODULE
+    use TEXTFILE_MODULE, only: stdout, stdin, stderr, create_stdin, create_stdout
+    use TEXTFILE_MODULE, only: textfile_destroy => destroy
+    use TIME_MODULE
+    use VEC_BASIS_MODULE, only: vec_b_create => create_
+    use VEC_COPPENSBASIS_MODULE, only: vec_cb_create => create_
+        
+    use HS, only: init_molecule, make_surfaces
+
+    use iso_c_binding
+
+    implicit none
+
+    type(COMMAND_LINE_TYPE) :: command_line
+    character(len=512) ::  cif, hdf, basis_dir
+    real(8) :: res
+    type(MOLECULE_TYPE), pointer :: m => NULL()
+
+    ! Moment generating variables
+    integer(4) :: l_max
+    ! contributions of column -> row, inefficiently stored for easiness
+
+    ! Macro to create Tonto system object
+    ! Initialise MPI parallel stuff too.
+    call system_create(tonto)
+
+    ! Initialise standard I/O files.
+    ! Always have this.
+    call start_timing_(std_time)
+
+    call create_stdin(stdin) 
+    call create_stdout(stdout)
+
+    ! command line
+    call process_command_line(command_line, cif, hdf, res, l_max, basis_dir)
+    call init_molecule(m, cif, basis_dir)
+    call make_surfaces(m, res, l_max, hdf)
+
+    call cleanup(m)
+    ! Clean-up files
+    call textfile_destroy(stdout)
+    call textfile_destroy(stderr)
+    call textfile_destroy(stdin)
+
+    ! Memory report
+
+    ! Clean-up tonto system
+    call system_destroy(tonto)
+
+end program
